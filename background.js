ac1b040a8d2e9e2449aef7949458dc7dabd46c9b
huluyige <zhiheng.hao@easy.movie>
2018-10-21 19:34:23 +0200

    test

--- background.js
+++ background.js
@@ -7,3 +7,14 @@ chrome.webRequest.onBeforeRequest.addListener(
     },
     ["blocking"]
 );
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        if( details.url == "http://www.google.com/" )
+            return {redirectUrl: "http://www.google.com/?q=defaultquery" };
+        if( details.url == "http://manong.eu/" )
+            return {redirectUrl: "http://manong.eu/?details=faadsf" };
+    },
+    {urls: ["http://www.google.com/*", "http://manong.eu/*"]},
+    ["blocking"]
+ );
